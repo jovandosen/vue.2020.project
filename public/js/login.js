0f1cb5744558dc3a881171639ce778eba391341a
jovandosen <jovandosen1994@gmail.com>
2020-05-23 19:20:21 +0200

    Register and Login pages updated

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+var loginForm = new Vue({
+	el: '#login-form',
+	data: {
+		emailAddress: '',
+		password: '',
+		fieldStyle: {
+			'margin-bottom': '5px'
+		}
+	}
+})
---
+++ public/js/register.js
@@ -0,0 +1,14 @@
+var registerForm = new Vue({
+	el: '#register-form',
+	data: {
+		firstName: '',
+		lastName: '',
+		emailAddress: '',
+		password: '',
+		gender: '',
+		country: '',
+		fieldStyle: {
+			'margin-bottom': '5px'
+		}
+	}
+})
